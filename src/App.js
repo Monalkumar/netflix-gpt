e1c2b9f12c8f16db03697d01667e74812dcdfdfb
MonalKumar <monalshiv123@gmail.com>
2024-12-08 00:32:01 +0530

    hellow world

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-700">Hello</h1>
+      <h1 className="text-blue-700">Hello world</h1>
     </div>
   );
 }
